adef8daac50d397757826561de8568c01a6dbb04
Kelias <aleksi.gautier@gmail.com>
2020-05-22 19:19:09 +0200

    minor optimizations

--- computorV1.py
+++ computorV1.py
@@ -54,7 +54,7 @@ def print_reduced_eq(eq):
 			coef = coef if i == 0 else abs(coef)
 			coef = coef if coef - int(coef) > 0 else int(coef) 
 			print(coef, "*", "X^" + str(i), end='')
-			if i != len(eq) - 1 and not is_at_end(eq, i + 1):
+			if i != len(eq) - 1:
 				print(" + " if eq[i + 1] > 0 else " - ", end='')
 	print(" = 0")
 
@@ -63,6 +63,9 @@ if __name__ == "__main__":
 	reduced_form = []
 	for i in range(len(req)):
 		reduced_form.append(req[i] - leq[i])
+	while (reduced_form[-1] == 0):
+		reduced_form.pop(-1)
 	print_reduced_eq(reduced_form)
-	
-	#sys.exit('The polynomial degree is strictly greater than 2, I can\'t solve.')
+	print("Polynomial degree:", len(reduced_form) - 1)
+	if len(reduced_form) - 1 > 2:
+		print("The polynomial degree is strictly greater than 2, I can't solve.")
