a6865d1de5c469d8b60fa6f270a774471af4e2da
Kelias <aleksi.gautier@gmail.com>
2020-05-24 15:49:38 +0200

    added solver

--- computorV1.py
+++ computorV1.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 import numpy as np
 
 def get_highest_power(str):
@@ -58,14 +58,29 @@ def print_reduced_eq(eq):
 				print(" + " if eq[i + 1] > 0 else " - ", end='')
 	print(" = 0")
 
+def solve(eq):
+	while len(eq) < 3:
+		eq.append(0)
+	a, b, c = eq[0], eq[1], eq[2]
+	delta = b**2 - 4*a*c
+	if delta == 0:
+		return -b / 2*a
+	elif delta > 0:
+		return [(-b + math.sqrt(delta)) / 2*a, (-b - math.sqrt(delta)) / 2*a]
+	else:
+		return [f"{-b} + i * {math.sqrt(abs(delta))} / {2*a}", f"{-b} - i * {math.sqrt(abs(delta))} / {2*a}"]
+
 if __name__ == "__main__":
 	req, leq = parse()
 	reduced_form = []
 	for i in range(len(req)):
 		reduced_form.append(req[i] - leq[i])
-	while (reduced_form[-1] == 0):
+	while reduced_form[-1] == 0:
 		reduced_form.pop(-1)
 	print_reduced_eq(reduced_form)
 	print("Polynomial degree:", len(reduced_form) - 1)
 	if len(reduced_form) - 1 > 2:
 		print("The polynomial degree is strictly greater than 2, I can't solve.")
+	else:
+		sol = solve(reduced_form)
+	print(f"Solution{'s are' if len(sol) > 1 else ' is'}:\n{sol}")
